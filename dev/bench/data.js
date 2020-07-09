c904168b5686a2f4b640d1cbfb278c8a1a12505b
github-action-benchmark <github@users.noreply.github.com>
2020-07-09 02:55:34 +0000

    add benches (cargo) benchmark result for d5bafa99b1a7bbfc64be6bb41754afded1610b95

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1594263334329,
+  "repoUrl": "https://github.com/msalib/spatial-join",
+  "entries": {
+    "benches": [
+      {
+        "commit": {
+          "author": {
+            "email": "msalib@alum.mit.edu",
+            "name": "Michael Salib",
+            "username": "msalib"
+          },
+          "committer": {
+            "email": "msalib@alum.mit.edu",
+            "name": "Michael Salib",
+            "username": "msalib"
+          },
+          "distinct": true,
+          "id": "d5bafa99b1a7bbfc64be6bb41754afded1610b95",
+          "message": "sigh",
+          "timestamp": "2020-07-08T22:46:45-04:00",
+          "tree_id": "f979b6dd09db1d77daa82536d4c641000a430a11",
+          "url": "https://github.com/msalib/spatial-join/commit/d5bafa99b1a7bbfc64be6bb41754afded1610b95"
+        },
+        "date": 1594263333781,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "1k_load",
+            "value": 719495,
+            "range": "± 3410",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "1k_self_spatial_join",
+            "value": 18605369,
+            "range": "± 12466",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
